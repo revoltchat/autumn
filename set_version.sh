2080ddebe6fc3ce3a717cd1dd13f9f9f6a8d4914
Paul <paulmakles@gmail.com>
2021-08-05 22:13:40 +0100

    Change banner limit from 4mb to 6mb.

--- set_version.sh
+++ set_version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export version=1.1.1
+export version=1.1.1-patch.0
 echo "pub const VERSION: &str = \"${version}\";" > src/version.rs
--- src/version.rs
+++ src/version.rs
@@ -1,1 +1,1 @@
-pub const VERSION: &str = "1.1.1";
+pub const VERSION: &str = "1.1.1-patch.0";
