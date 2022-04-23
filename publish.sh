d066ed94a9fb3876bd62ee6e4adcd0f8cf9fc4bc
bree <me@bree.dev>
2022-04-23 09:54:24 -0400

    refactor: use `CARGO_PKG_VERSION` and remove unneeded docker versioning scripts

--- publish.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-source set_version.sh
-
-docker build -t revoltchat/autumn:${version} . &&
-    docker push revoltchat/autumn:${version}
--- set_version.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export version=1.1.2
-echo "pub const VERSION: &str = \"${version}\";" > src/version.rs
